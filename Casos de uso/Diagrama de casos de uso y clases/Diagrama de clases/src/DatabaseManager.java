c3049a02970553b3740748d0d4793ba3724b4b73
Alvaro Rodriguez Garcia <alvarorgtr@gmail.com>
2016-04-08 13:35:24 +0200

    Modificado diagrama de clases

---
+++ Casos de uso/Diagrama de casos de uso y clases/Diagrama de clases/src/DatabaseManager.java
@@ -0,0 +1,22 @@
+
+public class DatabaseManager {
+
+	private boolean abierta;
+
+	public Empleado consultarEmpleado(int idEmpleado) {
+	
+	}
+
+	public void abrirDatabase() {
+	
+	}
+
+	public void añadirEmpleado(Empleado empleado) {
+	
+	}
+
+	public void eliminarEmpleado(int idEmpleado) {
+	
+	}
+
+}
