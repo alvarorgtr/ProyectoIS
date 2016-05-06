e74d4d94738009c4519f71ca97aac2e3dc6e788e
2ivan2 <batallon96@yahoo.es>
2016-05-06 13:34:19 +0200

    Controlador ya implementa interfaz

--- Segundo cuatrimestre/Diagramas/src/empleado/capadepresentacion/controlador/Controlador.java
+++ Segundo cuatrimestre/Diagramas/src/empleado/capadepresentacion/controlador/Controlador.java
@@ -1,12 +1,98 @@
 package empleado.capadepresentacion.controlador;
 
+import empleado.capadepresentacion.vista.gestoreventos.VistaAniadirEmpleadoListener;
+import empleado.capadepresentacion.vista.gestoreventos.VistaCambioContratosListener;
+import empleado.capadepresentacion.vista.gestoreventos.VistaCambioDepartamentoListener;
+import empleado.capadepresentacion.vista.gestoreventos.VistaEliminarEmpleadoListener;
+import empleado.capadepresentacion.vista.gestoreventos.VistaEspecificarBajaListener;
+import empleado.capadepresentacion.vista.gestoreventos.VistaFichaEmpleadoListener;
 import empleado.capadepresentacion.vista.gestoreventos.VistaListaEmpleadosListener;
+import empleado.capadepresentacion.vista.gestoreventos.VistaTrasladoFacultadListener;
 
-public class Controlador implements VistaListaEmpleadosListener {
+public class Controlador implements VistaAniadirEmpleadoListener,
+		VistaCambioContratosListener, VistaCambioDepartamentoListener,
+		VistaEliminarEmpleadoListener, VistaEspecificarBajaListener,
+		VistaFichaEmpleadoListener, VistaListaEmpleadosListener,
+		VistaTrasladoFacultadListener {
+
+	@Override
+	public void trasladoFacultad() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
 
 	@Override
 	public void empleadoSeleccionado(int idEmpleado) {
-		// TODO: pedir empleado
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void cambiarContratoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void especificarBajaPulsado() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void especificarTrasladoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void especificarCambioDepartamentoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void eliminarFicheroPulsado() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void darDeBaja() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void cancelarBaja() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void eliminarEmpleadoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void cambioDepartamento() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void cotratoCambiado() {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+	@Override
+	public void aniadirEmpleadoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+
 	}
 
 }
+
+
