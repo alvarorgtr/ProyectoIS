2c06fe8b85b581b48ceb518cb70aaec83c24d44a
Alvaro Rodriguez Garcia <alvarorgtr@gmail.com>
2016-05-13 13:47:23 +0200

    Añadidos metodos a controlador

--- Segundo cuatrimestre/Diagramas/src/empleado/capadepresentacion/controlador/Controlador.java
+++ Segundo cuatrimestre/Diagramas/src/empleado/capadepresentacion/controlador/Controlador.java
@@ -1,5 +1,9 @@
 package empleado.capadepresentacion.controlador;
 
+import usuario.capadenegocio.TipoFacultad;
+import empleado.capadenegocio.Contrato;
+import empleado.capadenegocio.Empleado;
+import empleado.capadenegocio.TipoBaja;
 import empleado.capadepresentacion.vista.gestoreventos.VistaAniadirEmpleadoListener;
 import empleado.capadepresentacion.vista.gestoreventos.VistaCambioContratosListener;
 import empleado.capadepresentacion.vista.gestoreventos.VistaCambioDepartamentoListener;
@@ -14,6 +18,84 @@ public class Controlador implements VistaAniadirEmpleadoListener,
 		VistaEliminarEmpleadoListener, VistaEspecificarBajaListener,
 		VistaFichaEmpleadoListener, VistaListaEmpleadosListener,
 		VistaTrasladoFacultadListener {
+
+	@Override
+	public void trasladoFacultad(TipoFacultad facultad) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void empleadoSeleccionado(int idEmpleado) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void cambiarContratoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void especificarBajaPulsado() {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void especificarTrasladoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void especificarCambioDepartamentoPulsado() {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void eliminarFicheroPulsado() {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void darDeBaja(int idEmpleado, TipoBaja baja) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void cancelarBaja(int idEmpleado) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void eliminarEmpleadoPulsado(int id) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void cambioDepartamento(String departamento) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void cotratoCambiado(Contrato contrato) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
+
+	@Override
+	public void aniadirEmpleadoPulsado(Empleado empleado) {
+		// TODO Apéndice de método generado automáticamente
+		
+	}
 
 }
 
