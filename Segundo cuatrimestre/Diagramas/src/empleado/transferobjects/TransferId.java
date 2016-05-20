9819490c6850e0ef735576a51f5e2eafa1e29b5b
Alvaro Rodriguez Garcia <alvarorgtr@gmail.com>
2016-05-20 11:13:13 +0200

    Revert "Añadido diagrama capanegocio empleado y singleton de transferId y de transferListEmpleados"
    This reverts commit a6b54d8b856d685007c79e001298558d875edf7f.

--- Segundo cuatrimestre/Diagramas/src/empleado/transferobjects/TransferId.java
+++ Segundo cuatrimestre/Diagramas/src/empleado/transferobjects/TransferId.java
@@ -10,22 +10,9 @@ import java.io.Serializable;
  */
 public class TransferId implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	public static TransferId instanceTransferId;
-	
 	private int id;
 	
-	private TransferId (int id) {
-		instanceTransferId.id = id;
-	}
-	
-	public static TransferId getInstance(int id){
-		if(instanceTransferId == null){
-			instanceTransferId = new TransferId(id);
-		}
-		else{
-			instanceTransferId.id = id;
-		}
-		return instanceTransferId;
+	public TransferId (int id) {
+		
 	}
 }
--- Segundo cuatrimestre/Diagramas/src/empleado/transferobjects/TransferListEmpleados.java
+++ Segundo cuatrimestre/Diagramas/src/empleado/transferobjects/TransferListEmpleados.java
@@ -12,24 +12,11 @@ import java.util.List;
  */
 public class TransferListEmpleados implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
-	private static TransferListEmpleados instanceTransferListEmpleados;
-	
 	private List<TransferEmpleadoVista> listaEmpleados;
 	private int index;
 	
-	private TransferListEmpleados (int index) {
-		this.index = index;
-	}
-	
-	public static synchronized TransferListEmpleados getInstance(int index){
-		if(instanceTransferListEmpleados == null){
-			instanceTransferListEmpleados = new TransferListEmpleados(index);
-		}
-		else{
-			instanceTransferListEmpleados.index = index;
-		}
-		return instanceTransferListEmpleados;
+	public TransferListEmpleados (int index) {
+		
 	}
 	
 	public void addTransferEmpleadoVista(TransferEmpleadoVista empleado) {
