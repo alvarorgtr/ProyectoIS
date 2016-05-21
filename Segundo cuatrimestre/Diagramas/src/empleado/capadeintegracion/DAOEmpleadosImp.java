04a149f0c14a8dd940c4d610406d2837bb111762
MiguelPascual13 <miguepas@ucm.es>
2016-05-21 12:53:28 +0200

    Aplicación de Singelton en el DAOEmpleados

--- Segundo cuatrimestre/Diagramas/src/empleado/capadeintegracion/DAOEmpleadosImp.java
+++ Segundo cuatrimestre/Diagramas/src/empleado/capadeintegracion/DAOEmpleadosImp.java
@@ -8,6 +8,17 @@ import empleado.capadenegocio.transferencia.TransferListEmpleados;
 
 
 public class DAOEmpleadosImp implements DAOEmpleados {
+	
+	private static DAOEmpleadosImp instance;
+	
+	private DAOEmpleadosImp(){}
+	
+	public static DAOEmpleadosImp getInstance(){
+		if(instance == null){
+			instance = new DAOEmpleadosImp(); 
+		}
+		return instance;
+	}
 
 	@Override
 	public Empleado consultarEmpleado(TransferInt idEmpleado) {
--- Segundo cuatrimestre/Diagramas/src/empleado/capadenegocio/logica/ImplementacionEmpleados.java
+++ Segundo cuatrimestre/Diagramas/src/empleado/capadenegocio/logica/ImplementacionEmpleados.java
@@ -10,38 +10,38 @@ public class ImplementacionEmpleados implements Empleados {
 
 	@Override
 	public void añadirEmpleado(TransferEmpleado transfer) {
-		DAOEmpleadosImp DAONegocio = new DAOEmpleadosImp();
+		DAOEmpleadosImp DAONegocio = DAOEmpleadosImp.getInstance();
 		DAONegocio.insertarEmpleado(transfer);
 	}
 
 	@Override
 	public TransferEmpleado perfilCompletoEmpleado(TransferInt transfer) {
-		DAOEmpleadosImp DAONegocio = new DAOEmpleadosImp();
+		DAOEmpleadosImp DAONegocio = DAOEmpleadosImp.getInstance();
 		TransferEmpleado empleadoTransfer = new TransferEmpleado(DAONegocio.consultarEmpleado(transfer));
 		return empleadoTransfer;
 	}
 
 	@Override
 	public TransferListEmpleados listaEmpleados(TransferInt pagina) {
-		DAOEmpleadosImp DAONegocio = new DAOEmpleadosImp();
+		DAOEmpleadosImp DAONegocio = DAOEmpleadosImp.getInstance();
 		return DAONegocio.getListempleadoVista(pagina);
 	}
 
 	@Override
 	public void eliminarEmpleado(TransferInt transfer) {
-		DAOEmpleadosImp DAONegocio = new DAOEmpleadosImp();
+		DAOEmpleadosImp DAONegocio = DAOEmpleadosImp.getInstance();
 		DAONegocio.eliminarEmpleado(transfer);
 	}
 	
 	@Override
 	public TransferListEmpleados buscarEmpleado(TransferBusqueda transfer) {
-		DAOEmpleadosImp DAONegocio = new DAOEmpleadosImp();
+		DAOEmpleadosImp DAONegocio = DAOEmpleadosImp.getInstance();
 		return DAONegocio.getListEmpleadoPorBusqueda(transfer);
 	}
 
 	@Override
 	public void modificarEmpleado(TransferEmpleado transfer) {
-		DAOEmpleadosImp DAONegocio = new DAOEmpleadosImp();
+		DAOEmpleadosImp DAONegocio = DAOEmpleadosImp.getInstance();
 		DAONegocio.modificarEmpleado(transfer);
 	}
 }
